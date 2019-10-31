b79a8cc538ecec644bcbe283406cbb7ca4928690
“dennisAlexander” <changeluhia@gmail.com>
2019-10-31 15:18:12 -0500

    fddsa

--- src/index.js
+++ src/index.js
@@ -2,19 +2,26 @@
 // allows to  code with a higher level of quality
 
 const express = require('express');
+const morgan = require('morgan');
 const app = express(); 
+// codigo del servidor  below
 
-//settings
+//settings             take this port from the  OS or default
+app.set('port', process.env.PORT || 3000)   // defining the value **
 
-// middlewares
+// middlewares           para entender formatos de intercambio, de datos
+app.use(morgan('dev'));   //gives a message in terminal from morgan
+app.use(express.json());    // when data passes the server, check for json, and send json data
 
-// routes                    .. urls the server uses
+// routes                     urls the server uses, tareas , connects with task.routes
 
-// Static files               .. to tell express where the statfiles are
+app.use('/api/tasks', require('./routes/task.routes'));
+
+// Static files                to tell express where the statfiles are
 
 
 //Starting the server
-
-app.listen(3000, () => {
-    console.log('server on port 3000');
+                                             // obtaining the value **
+app.listen(app.get('port'), () => {          // reveceive app.set
+    console.log(`server on port ${app.get('port')}`);
 });
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,1 +1,10 @@
 // define URL CRUD operations
+const express = require('express');
+const router = express.Router();   // method returns object in which routes can be injected
+
+// donde se define el REST API, donde va EL CRUD, donde todo se hace
+router.get('/', (req,res) => {    // when a request happens at root endpoint res console.log
+    res.json({
+        status: 'API Works!'
+    });
+})
